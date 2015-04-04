c3dd87a9a616cd308823100168e9c94c6e02a27e
Peter Chubb <peter.chubb@nicta.com.au>
2015-04-04 16:53:55 +1100

    Add comp;atibility functions
    Add set/getBrightness and US spelling to match ADAFruit interface.

--- ws2812.h
+++ ws2812.h
@@ -10,28 +10,55 @@ public:
     // Configure processor to output to data strip
     WS2812(uint16_t numLeds, uint8_t pin);
 
-    // Send colors to the strip and show them. Disables interrupts while processing.
+    // Send colours to the strip and show them.
+    // Disables interrupts while processing. 
     void show(void);
 
-    // Set the color of a certain LED
+    // Set the colour of a certain LED
     void setLEDColour(uint16_t p, uint8_t r, uint8_t g, uint8_t b);
 
-    // Clear the color of all LEDs (make them black/off)
+    // Clear the colour of all LEDs (make them black/off)
     void clear(void);
 
-    // Fill the strip with a solid color. This will update the strip.
+    // Fill the strip with a solid colour. This will update the strip.
     void fill(uint8_t r, uint8_t g, uint8_t b);
 
     // Pack RGB values into an int
-    uint32_t  Colour(uint8_t r, uint8_t g, uint8_t b);
+    static uint32_t Colour(uint8_t r, uint8_t g, uint8_t b);
 
     // Set a pixel to a packed colour
     void setLEDColour(uint16_t p, uint32_t c);
 
+    uint32_t getLEDColour(uint16_t p);
+
+    // Get/Set brightness
+    uint8_t getBrightness(void);
+    void setBrightness(uint8_t);
+
+    uint16_t numPixels(void);
+
+// Compatibility with AdaFruit NeoPixel library
+    inline void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
+        setLEDColour(n, r, g, b);
+    }
+
+    inline void setPixelColor(uint16_t n, uint32_t c) {
+        setLEDColour(n, c);
+    }
+
+    static inline uint32_t Color(uint8_t r, uint8_t g, uint8_t b) {
+        return Colour(r, g, b);
+    }
+
+    inline uint32_t getPixelColor(uint16_t n) {
+        return getLEDColour(n);
+    }
+
 private:
-    uint16_t pin;
     uint16_t numLeds;
+    uint16_t pin;
     uint16_t numBytes;
+    uint8_t brightness;
     uint8_t *leds;
 };
-    
+
